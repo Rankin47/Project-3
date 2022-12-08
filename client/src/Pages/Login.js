5068cc3b411463e26b36a7e40c18dee3adac6e19
ethugalge liyanage <mycanada123@gmail.com>
2022-12-08 00:21:53 -0500

    completed files

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -6,7 +6,7 @@ import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { useHistory } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ isLogedIn, setIsLogedIn }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -25,6 +25,11 @@ const Login = () => {
 
         Auth.login(data.login.token, data.login.user.username);
         // console.log(data);
+        if (data) {
+          if (!isLogedIn) {
+            setIsLogedIn(true);
+          }
+        }
         history.push("/");
       } catch (err) {
         console.error(err);
